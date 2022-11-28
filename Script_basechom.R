1bb4e90f8faa233627bdc605f23fc261795a669e
JulienBastian <julien.bastian@univ-lyon2.fr>
2022-11-28 16:27:34 +0100

    qques corrections recodage et write.table passés en com

--- Script_basechom.R
+++ Script_basechom.R
@@ -41,8 +41,9 @@ table(chomage2020$CSP)
 ## statut d 'activité 
 mode(chomage2020$ACTEU6)
 chomage2020$statutACT<-as.character(chomage2020$ACTEU6)
+chomage2020$statutACT=fct_explicit_na(chomage2020$statutACT, "null")
 chomage2020$statutACT<-fct_recode(chomage2020$statutACT,
-                              "Sans objet" = " ", ##non trouve par R - laisse la case vide - voir comment remplacer le vide par "sans objet" - mais n'empêche pas le pgm de tourner
+                              "Sans objet" = "null", ##non trouve par R - laisse la case vide - voir comment remplacer le vide par "sans objet" - mais n'empêche pas le pgm de tourner
                               "Actif occupé" = "1",
                               "Chômeur PSERE (Population sans Emploi à la Recherche d'un Emploi)"="3",
                               "Autre chômeur BIT"="4",
@@ -50,6 +51,7 @@ chomage2020$statutACT<-fct_recode(chomage2020$statutACT,
                               "Autres inactifs (dont retraités)"="6")
 
 ##verification
+sum(is.na(as.character(chomage2020$ACTEU6)))
 table(chomage2020$ACTEU6)
 table(chomage2020$statutACT)
 
@@ -59,12 +61,12 @@ table(chomage2020$statutACT)
 mode(chomage2020$AGE5)
 chomage2020$AGE<-as.character(chomage2020$AGE5)
 chomage2020$AGE<-fct_recode(chomage2020$AGE,
-                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 15 à 29 ans"="0" ## recodage inutile car CSP sans actif 
-                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 15 à 29 ans"="15"
-                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 30 à 39 ans"="30"
-                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 40 à 49 ans"="40"
-                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 50 à 59 ans "="40"
-                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) DE 60 ans ou plus"="60 ou plus"
+                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 15 à 29 ans"="0", ## recodage inutile car CSP sans actif 
+                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 15 à 29 ans"="15",
+                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 30 à 39 ans"="30",
+                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 40 à 49 ans"="40",
+                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) De 50 à 59 ans "="40",
+                            "Âge au dernier jour de la semaine de référence (5 postes, premier type de regroupement) DE 60 ans ou plus"="60 ou plus",)
 
 
 ##verification
@@ -76,9 +78,9 @@ table(chomage2020$AGE)
 mode(chomage2020$ACTIF)
 chomage2020$tauxdechômage<-as.character(chomage2020$ACTIF)
 chomage2020$tauxdechômage<-fct_recode(chomage2020$tauxdechômage,
-                                      "Actif au sens du BIT/Actif"="1"
-                                      "Actif au sens du BIT/inactif"="2"
-
+                                      "Actif au sens du BIT/Actif"="1",
+                                      "Actif au sens du BIT/inactif"="2",
+)
 
  ##verification
 table(chomage2020$ACTIF)
@@ -88,21 +90,23 @@ table(chomage2020$tauxdechômage)
 ## raison changement d'emploi (CREACCP)
 mode(chomage2020$CREACCP)
 chomage2020$raisonchangemploi<-as.character(chomage2020$CREACCP)
+chomage2020$raisonchangemploi=fct_explicit_na(chomage2020$raisonchangemploi, "null")
 chomage2020$raisonchangemploi<-fct_recode(chomage2020$raisonchangemploi,
-                                          "Raison principale de la recherche d'un autre emploiSans objet (SOUA distinct de 1 et SOUB distinct de 1) ou non renseigné"=""
-                                          "Raison principale de la recherche d'un autre emploi Risque de perdre ou va perdre son emploi actuel (y compris fin de contrats courts)"="1"
-                                          "Raison principale de la recherche d'un autre emploi Désire un emploi plus intéressant"="2"
-                                          "Raison principale de la recherche d'un autre emploi Veut un emploi plus stable (CDI)"="3"
-                                          "Raison principale de la recherche d'un autre emploi Veut travailler plus d'heures"="4"
-                                          "Raison principale de la recherche d'un autre emploi Désire un travail avec un rythme horaire plus adapté ou plus modulable"="5"
-                                          "Raison principale de la recherche d'un autre emploie Désire des conditions de travail moins pénibles ou plus adaptées à sa santé"="6"
-                                          "Raison principale de la recherche d'un autre emploi Désire augmenter ses revenus"="7"
-                                          "Raison principale de la recherche d'un autre emploi Désire diminuer son temps de transport"="8"
-                                          "Raison principale de la recherche d'un autre emploi Doit ou veut déménager"="9"
-                                          "Raison principale de la recherche d'un autre emploi veut s'installer à son compte"="10"
-                                          "Raison principale de la recherche d'un autre emploi Veut changer de métier ou de secteur"="11"
-                                         " Raison principale de la recherche d'un autre emploi Trouve l'ambiance de travail mauvaise, les relations de travail conflictuelles"="12"
-                                          "Raison principale de la recherche d'un autre emploi Autre raison"="13"
+                                          "Sans objet (SOUA distinct de 1 et SOUB distinct de 1) ou non renseigné"="null",
+                                          "Risque de perdre ou va perdre son emploi actuel (y compris fin de contrats courts)"="1",
+                                          "Désire un emploi plus intéressant"="2",
+                                          "Veut un emploi plus stable (CDI)"="3",
+                                          "Veut travailler plus d'heures"="4",
+                                          "Désire un travail avec un rythme horaire plus adapté ou plus modulable"="5",
+                                          "Désire des conditions de travail moins pénibles ou plus adaptées à sa santé"="6",
+                                          "Désire augmenter ses revenus"="7",
+                                          "Désire diminuer son temps de transport"="8",
+                                          "Doit ou veut déménager"="9",
+                                          "Veut s'installer à son compte"="10",
+                                          "Veut changer de métier ou de secteur"="11",
+                                          "Trouve l'ambiance de travail mauvaise, les relations de travail conflictuelles"="12",
+                                          "Autre raison"="13",
+)
                                           
 ##vérification 
 
@@ -116,27 +120,27 @@ table(chomage2020$raisonchangemploi)
 tmp<-table(chomage2020$CSP,chomage2020$statutACT)
 tmprop<-lprop(tmp)
 chisq.test(tmp) #p-value
-write.table(tmprop,file = "excel_provisoire.xls",sep = "\t")
+#write.table(tmprop,file = "excel_provisoire.xls",sep = "\t")
 
 ## tableau Age et taux de chômage
 
 tmp<-table(chomage2020$AGE,chomage2020$tauxdechômage)
 tmprop<-lprop(tmp)
 chisq.test(tmp) #p-value
-write.table(tmprop,file = "excel_tableau âge.xls",sep = "\t")
+#write.table(tmprop,file = "excel_tableau âge.xls",sep = "\t")
 
 ## tableau taux de chômage et statut d'activité 
 
 tmp<-table(chomage2020$tauxdechômage,chomage2020$statutACT)
 tmprop<-lprop(tmp)
 chisq.test(tmp) #p-value
-write.table(tmprop,file = "excel_statutact-chômage.xls",sep = "\t")
+#write.table(tmprop,file = "excel_statutact-chômage.xls",sep = "\t")
 
 ## tableau catégorie socio-pro et raison de changement d 'emploi 
 tmp<-table(chomage2020$CSP,chomage2020$raisonchangemploi)
 tmprop<-lprop(tmp)
 chisq.test(tmp) #p-value
-write.table(tmprop,file = "excel_tableauchangement d'emploi et csp .xls",sep = "\t")
+#write.table(tmprop,file = "excel_tableauchangement d'emploi et csp .xls",sep = "\t")
 
 ##tableau tri-varié age , csp,, taux de chômage  : taux de chômage en fonction de l'âge et de la csp 
 freq(chomage2020$AGE)
@@ -149,7 +153,7 @@ lprop(table(chomage2020$CSP,chomage2020$tauxdechômage))
 
 chisq.test(table(chomage2020$AGE5,chomage2020$tauxdechômage))
 chisq.test(table(chomage2020$CSP,chomage2020$tauxdechômage))
-write.table(tmprop,file = "excel_tableau taux de chomage age et csp .xls",sep = "\t")
+#write.table(tmprop,file = "excel_tableau taux de chomage age et csp .xls",sep = "\t")
 
 
 ##tableau tri-varié : sexe , csp et taux de chomâge : taux de chômage en fonction du sexe et de la csp 
@@ -178,7 +182,7 @@ lprop(table(chomage2020$AGE5,chomage2020$tauxdechômage))
 
 chisq.test(table(chomage2020$SEXE,chomage2020$tauxdechômage))
 chisq.test(table(chomage2020$AGE5,chomage2020$tauxdechômage))
-write.table(tmprop,file = "excel_tableautaux de chomage sexe et age .xls",sep = "\t")
+#write.table(tmprop,file = "excel_tableautaux de chomage sexe et age .xls",sep = "\t")
 
 
  ## tableau tri varié : statut d 'act,, taux de chômage et csp 
@@ -192,7 +196,7 @@ lprop(table(chomage2020$CSP,chomage2020$tauxdechômage))
 
 chisq.test(table(chomage2020$statutACT,chomage2020$tauxdechômage))
 chisq.test(table(chomage2020$CSP,chomage2020$tauxdechômage))
-write.table(tmprop,file = "excel_tableautaux de chomage csp et statut act  .xls",sep = "\t")
+#write.table(tmprop,file = "excel_tableautaux de chomage csp et statut act  .xls",sep = "\t")
 
 
 
