99fd68f2c06ed42ba61aee104358a74a9e938027
JulienBastian <julien.bastian@univ-lyon2.fr>
2022-11-30 12:30:51 +0100

    dep

--- Script_basechom.R
+++ Script_basechom.R
@@ -1,64 +1,35 @@
-##base de données chomage 2020 
-## chargement de la base de donnée 
+##base de donnÃ©es chomage 2020 
+## chargement de la base de donnÃ©e 
 chomage2020 <-read.csv("FD_csv_EEC20.csv",sep = ";")
-
-#library(esquisse)
-
-##Mettre en français 
-#iset_i18n("fr")
-#esquisser(chomage2020)
-##lancement des librairies
 library(questionr)
 library(tidyverse)
-#library(ggplot2)
 library(FactoMineR)
 
-## I- recodage de la base de données ----
+chomage2020=as_tibble(chomage2020)
 
-## catégorie socio -professionnelle
-as_tibble(chomage2020)
-chomage2020
-colnames(chomage2020)
-mode(chomage2020$CSTOTR)
 chomage2020$CSP<-as.character(chomage2020$CSTOTR)
-chomage2020$CSP<-fct_recode(chomage2020$CSP,
-                            "Non renseigné"="0",
-                            "Agriculteurs exploitants"="1",
-                            "Artisans, commerçants, et chefs d'entreprise"="2",
-                            "Cadres et professions intellectuelles supérieures"="3",
-                            "Professions intermédiaires"="4",
-                            "Employés"="5",
-                            "Ouvriers"="6",
-                            "Inactifs avec activité professionnelle antérieure"="7",
-                            "Autres personnes sans activité professionnelle"="8" )
+#chomage2020$CSP<-fct_recode(chomage2020$CSP,
+#                            "Non renseigné"="0",
+#                           "Agriculteurs exploitants"="1",
+#                            "Artisans, commerçants, et chefs d'entreprise"="2",
+#                            "Cadres et professions intellectuelles supérieures"="3",
+#                            "Professions intermédiaires"="4",
+#                            "Employés"="5",
+#                            "Ouvriers"="6",
+#                            "Inactifs avec activité professionnelle antérieure"="7",
+#                            "Autres personnes sans activité professionnelle"="8" )
 
-
-
-##vérification 
-table(chomage2020$CSTOTR)
-table(chomage2020$CSP)
-
-## statut d 'activité 
-mode(chomage2020$ACTEU6)
 chomage2020$statutACT<-as.character(chomage2020$ACTEU6)
-chomage2020$statutACT=fct_explicit_na(chomage2020$statutACT, "null")
-chomage2020$statutACT<-fct_recode(chomage2020$statutACT,
-                                  "Sans objet" = "null", ##non trouve par R - laisse la case vide - voir comment remplacer le vide par "sans objet" - mais n'empêche pas le pgm de tourner
-                                  "Actif occupé" = "1",
-                                  "Chômeur PSERE (Population sans Emploi à la Recherche d'un Emploi)"="3",
-                                  "Autre chômeur BIT"="4",
-                                  "Étudiant, élève, stagiaire en formation (inactifs)"="5",
-                                  "Autres inactifs (dont retraités)"="6")
+#chomage2020$statutACT=fct_explicit_na(chomage2020$statutACT, "null")
+#chomage2020$statutACT<-fct_recode(chomage2020$statutACT,
+#                                  "Sans objet" = "null",
+#                                  "Actif occupé" = "1",
+#                                  "Chômeur PSERE (Population sans Emploi à la Recherche d'un Emploi)"="3",
+#                                  "Autre chômeur BIT"="4",
+#                                  "Étudiant, élève, stagiaire en formation (inactifs)"="5",
+#                                  "Autres inactifs (dont retraités)"="6")
+#
 
-##verification
-sum(is.na(as.character(chomage2020$ACTEU6)))
-table(chomage2020$ACTEU6)
-table(chomage2020$statutACT)
-
-
-## tranche d'âge (AGE5)
-
-mode(chomage2020$AGE5)
 chomage2020$AGE<-as.character(chomage2020$AGE5)
 #age au dernier jour de la semaine de référence
 chomage2020$AGE<-fct_recode(chomage2020$AGE,
@@ -70,27 +41,17 @@ chomage2020$AGE<-fct_recode(chomage2020$AGE,
                             "60 ans ou +"="60",)
 
 
-##verification
-
-table(chomage2020$AGE5)
-table(chomage2020$AGE)
-
 ## Actif ou non actif (ACTIF)
 
-mode(chomage2020$ACTIF)
 chomage2020$tauxdechômage<-as.character(chomage2020$ACTIF)
 chomage2020$tauxdechômage<-fct_recode(chomage2020$tauxdechômage,
                                       "Actif au sens du BIT/Actif"="1",
                                       "Actif au sens du BIT/inactif"="2",
 )
 
-##verification
-table(chomage2020$ACTIF)
-table(chomage2020$tauxdechômage)
 
 
 ## raison changement d'emploi (CREACCP)
-mode(chomage2020$CREACCP)
 chomage2020$raisonchangemploi<-as.character(chomage2020$CREACCP)
 chomage2020$raisonchangemploi=fct_explicit_na(chomage2020$raisonchangemploi, "null")
 chomage2020$raisonchangemploi<-fct_recode(chomage2020$raisonchangemploi,
@@ -110,113 +71,111 @@ chomage2020$raisonchangemploi<-fct_recode(chomage2020$raisonchangemploi,
                                           "Autre raison"="13",
 )
 chomage2020=sample_n(chomage2020, 1000)
-##vérification 
-
 table(chomage2020$CREACCP)
 table(chomage2020$raisonchangemploi)
 
 
 ##II-Analyse de la base----
-## tableau CSP et statut d 'activité 
+## tableau CSP et statut d 'activitÃ© 
 
 tmp<-table(chomage2020$CSP,chomage2020$statutACT)
 tmprop<-lprop(tmp)
 chisq.test(tmp) #p-value
 #write.table(tmprop,file = "excel_provisoire.xls",sep = "\t")
 
-## tableau Age et taux de chômage
+## tableau Age et taux de chÃ´mage
 
 tmp<-table(chomage2020$AGE,chomage2020$tauxdechômage)
 tmprop<-lprop(tmp)
 chisq.test(tmp) #p-value
-#write.table(tmprop,file = "excel_tableau âge.xls",sep = "\t")
+#write.table(tmprop,file = "excel_tableau Ã¢ge.xls",sep = "\t")
 
-## tableau taux de chômage et statut d'activité 
+## tableau taux de chÃ´mage et statut d'activitÃ© 
 
-tmp<-table(chomage2020$tauxdechômage,chomage2020$statutACT)
+tmp<-table(chomage2020$tauxdechÃ´mage,chomage2020$statutACT)
 tmprop<-lprop(tmp)
 chisq.test(tmp) #p-value
-#write.table(tmprop,file = "excel_statutact-chômage.xls",sep = "\t")
+#write.table(tmprop,file = "excel_statutact-chÃ´mage.xls",sep = "\t")
 
-## tableau catégorie socio-pro et raison de changement d 'emploi 
+## tableau catÃ©gorie socio-pro et raison de changement d 'emploi 
 tmp<-table(chomage2020$CSP,chomage2020$raisonchangemploi)
 tmprop<-lprop(tmp)
 chisq.test(tmp) #p-value
 #write.table(tmprop,file = "excel_tableauchangement d'emploi et csp .xls",sep = "\t")
 
-##tableau tri-varié age , csp,, taux de chômage  : taux de chômage en fonction de l'âge et de la csp 
+##tableau tri-variÃ© age , csp,, taux de chÃ´mage  : taux de chÃ´mage en fonction de l'Ã¢ge et de la csp 
 freq(chomage2020$AGE)
 freq(chomage2020$CSP)
-freq(chomage2020$tauxdechômage)
+freq(chomage2020$tauxdechÃ´mage)
 
-##tir croisé 
-lprop(table(chomage2020$AGE5,chomage2020$tauxdechômage))
-lprop(table(chomage2020$CSP,chomage2020$tauxdechômage))
+##tir croisÃ© 
+lprop(table(chomage2020$AGE5,chomage2020$tauxdechÃ´mage))
+lprop(table(chomage2020$CSP,chomage2020$tauxdechÃ´mage))
 
-chisq.test(table(chomage2020$AGE5,chomage2020$tauxdechômage))
-chisq.test(table(chomage2020$CSP,chomage2020$tauxdechômage))
+chisq.test(table(chomage2020$AGE5,chomage2020$tauxdechÃ´mage))
+chisq.test(table(chomage2020$CSP,chomage2020$tauxdechÃ´mage))
 #write.table(tmprop,file = "excel_tableau taux de chomage age et csp .xls",sep = "\t")
 
 
-##tableau tri-varié : sexe , csp et taux de chomâge : taux de chômage en fonction du sexe et de la csp 
+##tableau tri-variÃ© : sexe , csp et taux de chomÃ¢ge : taux de chÃ´mage en fonction du sexe et de la csp 
 
 freq(chomage2020$SEXE)
 freq(chomage2020$CSP)
-freq(chomage2020$tauxdechômage)
+freq(chomage2020$tauxdechÃ´mage)
 
-## tir croisé 
-lprop(table(chomage2020$SEXE,chomage2020$tauxdechômage))
-lprop(table(chomage2020$CSP,chomage2020$tauxdechômage))
+## tir croisÃ© 
+lprop(table(chomage2020$SEXE,chomage2020$tauxdechÃ´mage))
+lprop(table(chomage2020$CSP,chomage2020$tauxdechÃ´mage))
 
-chisq.test(table(chomage2020$SEXE,chomage2020$tauxdechômage))
-chisq.test(table(chomage2020$CSP,chomage2020$tauxdechômage))
+chisq.test(table(chomage2020$SEXE,chomage2020$tauxdechÃ´mage))
+chisq.test(table(chomage2020$CSP,chomage2020$tauxdechÃ´mage))
 #write.table(tmprop,file = "excel_tableautaux de chomage sexe et csp .xls",sep = "\t")
 
-##tableau tri-varié : sexe , age et taux de chomâge : taux de chômage en fonction du sexe et de l 'âge 
+##tableau tri-variÃ© : sexe , age et taux de chomÃ¢ge : taux de chÃ´mage en fonction du sexe et de l 'Ã¢ge 
 
 freq(chomage2020$SEXE)
 freq(chomage2020$AGE5)
-freq(chomage2020$tauxdechômage)
+freq(chomage2020$tauxdechÃ´mage)
 
-## tir croisé 
-lprop(table(chomage2020$SEXE,chomage2020$tauxdechômage))
-lprop(table(chomage2020$AGE5,chomage2020$tauxdechômage))
+## tir croisÃ© 
+lprop(table(chomage2020$SEXE,chomage2020$tauxdechÃ´mage))
+lprop(table(chomage2020$AGE5,chomage2020$tauxdechÃ´mage))
 
-chisq.test(table(chomage2020$SEXE,chomage2020$tauxdechômage))
-chisq.test(table(chomage2020$AGE5,chomage2020$tauxdechômage))
+chisq.test(table(chomage2020$SEXE,chomage2020$tauxdechÃ´mage))
+chisq.test(table(chomage2020$AGE5,chomage2020$tauxdechÃ´mage))
 #write.table(tmprop,file = "excel_tableautaux de chomage sexe et age .xls",sep = "\t")
 
 
-## tableau tri varié : statut d 'act,, taux de chômage et csp 
+## tableau tri variÃ© : statut d 'act,, taux de chÃ´mage et csp 
 freq(chomage2020$CSP)
 freq(chomage2020$statutACT)
-freq(chomage2020$tauxdechômage)
+freq(chomage2020$tauxdechÃ´mage)
 
-## tir croisé 
-lprop(table(chomage2020$statutACT,chomage2020$tauxdechômage))
-lprop(table(chomage2020$CSP,chomage2020$tauxdechômage))
+## tir croisÃ© 
+lprop(table(chomage2020$statutACT,chomage2020$tauxdechÃ´mage))
+lprop(table(chomage2020$CSP,chomage2020$tauxdechÃ´mage))
 
-chisq.test(table(chomage2020$statutACT,chomage2020$tauxdechômage))
-chisq.test(table(chomage2020$CSP,chomage2020$tauxdechômage))
+chisq.test(table(chomage2020$statutACT,chomage2020$tauxdechÃ´mage))
+chisq.test(table(chomage2020$CSP,chomage2020$tauxdechÃ´mage))
 #write.table(tmprop,file = "excel_tableautaux de chomage csp et statut act  .xls",sep = "\t")
 
 ## III- qques plots
 
 ggplot(chomage2020)+aes(x=statutACT)+geom_bar(aes(fill=statutACT))
 
-#ggplot(chomage2020)+geom_bar(aes(x = statutACT, fill=tauxdechômage))
+#ggplot(chomage2020)+geom_bar(aes(x = statutACT, fill=tauxdechÃ´mage))
 
-#ggplot(chomage2020)+geom_bar(aes(x = CSP,fill=tauxdechômage))
+#ggplot(chomage2020)+geom_bar(aes(x = CSP,fill=tauxdechÃ´mage))
 
 ggplot(chomage2020)+geom_bar(aes(x = CSP, fill=AGE))
 
 ggplot(chomage2020)+geom_bar(aes(x = CSP, fill=AGE), position="fill")
 
-ggplot(subset(chomage2020, statutACT == "Actif occupé")
+ggplot(subset(chomage2020, statutACT == "Actif occupÃ©")
 )+geom_bar(aes(x=statutACT, fill=raisonchangemploi))
 
-ggplot(subset(chomage2020, statutACT == "Chômeur PSERE (Population sans Emploi à la Recherche d'un Emploi)")
+ggplot(subset(chomage2020, statutACT == "ChÃ´meur PSERE (Population sans Emploi Ã  la Recherche d'un Emploi)")
 )+geom_bar(aes(x=AGE, fill=CSP))
 
-ggplot(subset(chomage2020, statutACT == "Actif occupé")
+ggplot(subset(chomage2020, statutACT == "Actif occupÃ©")
 )+geom_bar(aes(x=AGE, fill=CSP))
